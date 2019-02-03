9c46402fd9052be81dd594603f0f6f16c1e40517
chenshf666 <1795176826@qq.com>
2019-02-03 17:33:33 +0800

    no many changes but just use flexible

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var left_button = document.getElementById("left_button");
 var right_button = document.getElementById("right_button");
-var left_div_height = 90;
+var left_div_height = 20;
 left_button.addEventListener("click",increaseLeft);
 right_button.addEventListener("click",increaseRight);
 
@@ -9,12 +9,11 @@ function increaseLeft(){
 	var new_div =  document.createElement("div");
 	new_div.className = "left_intern_box";
 	new_div.style = "margin:10px 10% 10px 10%; background-color: white; width:80%;";
-	new_div.style.height = left_div_height + "%";
+	new_div.style.minHeight = left_div_height + "px";
+	new_div.style.height = "90%";
 	left_div.appendChild(new_div);
-	console.log(left_div.clientHeight);
-	console.log(left_div.scrollHeight);
 
-	if(left_div.clientHeight < left_div.scrollHeight){
+	/*if(left_div.clientHeight < left_div.scrollHeight){
 		var arr = document.getElementsByClassName("left_intern_box");
 		console.log(arr);
 		var dHeight = parseInt(window.getComputedStyle(left_div).height);
@@ -22,7 +21,7 @@ function increaseLeft(){
 		for(var i = 0; i < arr.length; i++){
 			arr[i].style.height = left_div_height+ "%";
 		}
-	}
+	}*/
 }
 
 function increaseRight(){
